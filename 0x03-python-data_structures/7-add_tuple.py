3fc408b66eabb2ea6940d7c56609f2cbf5b98e2d
macjayz <macjayzz1@gmail.com>
2023-03-14 14:23:58 -0700

    New code added

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,11 +3,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # If tuple_a or tuple_b have less than 2 elements, add a 0 to the end
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-    
+
     # Take only the first 2 elements of the tuples
     a, b = tuple_a[:2]
     c, d = tuple_b[:2]
-    
+
     # Add the elements together and return the resulting tuple
     return (a+c, b+d)
-

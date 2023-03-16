6962b8cbe2de1cd4a744396245c69979093c1d8a
macjayz <macjayzz1@gmail.com>
2023-03-16 16:21:23 -0700

    New code added

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
-    def find_search(element):
-        return element if element != search else replace
-    return list(map(find_search, my_list))
+    return list(map(lambda element: replace if element ==
+                search else element, my_list))

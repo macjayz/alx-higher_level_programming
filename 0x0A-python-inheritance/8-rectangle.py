44bc042696892322faca64b58f432edffb8e85ed
macjayz <macjayzz1@gmail.com>
2023-04-11 15:28:29 -0700

    New commit

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" a function checks for instance """
+
+
+class BaseGeometry:
+    """ Base class """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+class Rectangle(BaseGeometry):
+    """ This class inherits from base class """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height

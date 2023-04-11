dc4baa9e6b35b1b1415c6d21ecd2c72afac7399e
macjayz <macjayzz1@gmail.com>
2023-04-11 10:55:10 -0700

    New commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" This checks for available attributes """
-
-
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object
     """
-    return dir(obj)
+    attrs = []
+    for attr in dir(obj):
+        attrs.append(attr)
+    return attrs

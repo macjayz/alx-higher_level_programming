efb72ec4da1ee34fcb2d845a08ea2d6d7de25216
macjayz <macjayzz1@gmail.com>
2023-04-11 14:55:30 -0700

    New commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a function checks for instance """
+
+
+class BaseGeometry:
+    """ This class does nothing """
+    def area(self):
+        raise Exception("area() is not implemented")
--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-is_kind_of_class = __import__('3-is_kind_of_class').is_kind_of_class
+BaseGeometry = __import__('6-base_geometry').BaseGeometry
 
-a = 1
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
-if is_kind_of_class(a, float):
-    print("{} comes from {}".format(a, float.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
+bg = BaseGeometry()
 
+try:
+    print(bg.area())
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))

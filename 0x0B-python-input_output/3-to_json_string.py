9b318ef308b53487472a37ed89683a801c9feed5
macjayz <macjayzz1@gmail.com>
2023-04-12 14:25:30 -0700

    New code added

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+""" This function will return JSON """
+
+
+def to_json_string(my_obj):
+    """ This returns JSON representation of string """
+    return json.dumps(my_obj)

b1d4c7c8df06abf3e26958b2f599bb7a63f57ff5
macjayz <macjayzz1@gmail.com>
2023-04-12 15:46:33 -0700

    New code added

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This prints a pascal triangle"""
+
+
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1] * (i+1)
+        for j in range(1, i):
+            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        triangle.append(row)
+    return triangle

6ab1fc96a89a9267dbeaca307cc67cfe0b00aa75
macjayz <macjayzz1@gmail.com>
2023-04-12 15:57:55 -0700

    New code added

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" this reads stdin line by line """
+import sys
+
+
+def print_stats(size, status_codes):
+    """Print accumulated metrics
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+size = 0
+status_codes = {}
+valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+
+for line in sys.stdin:
+    line = line.split()
+
+    try:
+        size += int(line[-1])
+    except (IndexError, ValueError):
+        pass
+
+    try:
+        if line[-2] in valid_codes:
+            status_codes[line[-2]] = status_codes.get(line[-2], 0) + 1
+    except IndexError:
+        pass
+
+print_stats(size, status_codes)

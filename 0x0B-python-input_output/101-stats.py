cef2f4c7ad99a07f73aee34eaec662625115919d
macjayz <macjayzz1@gmail.com>
2023-04-13 14:00:47 -0700

    New code added

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,23 +10,23 @@ def print_stats(size, status_codes):
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
+if __name__ == "__main__":
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
-size = 0
-status_codes = {}
-valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    for line in sys.stdin:
+        line = line.split()
 
-for line in sys.stdin:
-    line = line.split()
+        try:
+            size += int(line[-1])
+        except (IndexError, ValueError):
+            pass
 
-    try:
-        size += int(line[-1])
-    except (IndexError, ValueError):
-        pass
+        try:
+            if line[-2] in valid_codes:
+                status_codes[line[-2]] = status_codes.get(line[-2], 0) + 1
+        except IndexError:
+            pass
 
-    try:
-        if line[-2] in valid_codes:
-            status_codes[line[-2]] = status_codes.get(line[-2], 0) + 1
-    except IndexError:
-        pass
-
-print_stats(size, status_codes)
+    print_stats(size, status_codes)

b90e699081998419ac1e37a0e3772c3130e5b0d7
macjayz <macjayzz1@gmail.com>
2023-06-10 07:23:28 -0700

    New commit

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,7 @@
 --  a script that updates the score of Bob to 10 in the table second_table.
 
-UPDATE second_table SET score = 10 WHERE name = "Bob";
+UPDATE second_table
+SET
+score = 10
+WHERE
+name = Bob;

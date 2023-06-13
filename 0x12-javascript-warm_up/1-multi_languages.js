04ec7c8478c886ff8387cfcf217e23a3651788e7
macjayz <macjayzz1@gmail.com>
2023-06-13 07:06:15 -0700

    Additing new javascript DOM

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/bin/node
+
+const first = 'C is fun';
+const second = 'Python is cool';
+const third = 'Javascript is amazing';
+const all = first + "\n" + second + "\n" + third;
+console.log(all);

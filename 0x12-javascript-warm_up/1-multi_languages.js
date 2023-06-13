bdd074bfdd412dcb3c9d0db82ec0d63812525a53
macjayz <macjayzz1@gmail.com>
2023-06-13 07:11:57 -0700

    Additing new javascript DOM

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/bin/node
 
-const first = 'C is fun';
-const second = 'Python is cool';
-const third = 'JavaScript is amazing';
-const all = first + '\n' + second + '\n' + third;
-console.log(all);
+const Myvar = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(Myvar);

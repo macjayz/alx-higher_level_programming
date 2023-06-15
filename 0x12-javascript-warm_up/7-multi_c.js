d409758327a3bb783e74d6a62e0b719cc9fca23e
macjayz <macjayzz1@gmail.com>
2023-06-15 06:48:04 -0700

    Additing new javascript DOM

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,6 +3,7 @@ const length = parseInt(process.argv[2]);
 if (!length) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < length; i++){
+  for (let i = 0; i < length; i++) {
     console.log('C is fun');
+  }
 }

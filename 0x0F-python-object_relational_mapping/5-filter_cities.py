09737ca0f6a430e968253f8f01afc04360282a97
macjayz <macjayzz1@gmail.com>
2023-06-19 15:48:06 -0700

    New commit

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     """
 
     db = db.connect(host="localhost", port=3306,
-                     user=argv[1], passwd=argv[2], db=argv[3])
+                    user=argv[1], passwd=argv[2], db=argv[3])
 
     with db.cursor() as cursor:
         cursor.execute("""

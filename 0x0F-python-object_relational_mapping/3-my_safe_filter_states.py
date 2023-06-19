54421982bbc0062aa67921bc63ca3c73aa27141d
macjayz <macjayzz1@gmail.com>
2023-06-19 15:29:27 -0700

    New commit

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     displays all values from the database.
     """
     db = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+                    user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute(
         "SELECT * FROM states WHERE name LIKE \

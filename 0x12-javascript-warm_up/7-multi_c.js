bd0bae1aa5d21002491ef073eafee04c22cfe4e3
Machen John <macjayzz1@gmail.com>
2023-09-12 11:20:22 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const length = parseInt(process.argv[2]);
-if (!length) {
-  console.log('Missing number of occurrences');
-} else {
-  for (let i = 0; i < length; i++) {
-    console.log('C is fun');
-  }
-}
